788d5a43121d11af5202878a16f79ee10a8e5848
Rafael <rafalauton@gmail.com>
2022-10-12 12:11:38 -0300

    new-add

--- main.c
+++ main.c
@@ -32,6 +32,7 @@ int main() {
             break;
 
         case 'L':
+            listAll();
             break;
         }
     }
--- people.c
+++ people.c
@@ -17,20 +17,114 @@ long int regPeople = 0;
 void add() {
     char name[30], id[10];
     int age;
+    People auxPerson;
     printf("Name: ");
-    scanf(" %s", &name);
+    scanf(" %s", name);
     printf("Id: ");
-    scanf(" %s", &id);
+    scanf(" %s", id);
     printf("Age: ");
     scanf("%d", &age);
 
     if (PeopleArch != NULL) {
+        // procurar ponto de inserção
+        fseek(PeopleArch,0,0);
+        int pos;
+        do {
+            pos = ftell(PeopleArch);
+            fread(&person, SIZE, 1, PeopleArch);
+            if (strcmp(person.id, id) == 0) {
+                printf("Id already registered");
+                return;
+            }
+            if (person.age > age)
+                break;
+        }
+        while (!feof(PeopleArch));
+        
+        // ponto de inserção no fim do arquivo
+        if (feof(PeopleArch)) {
+            printf("INSERCAO NO FINAL\n");
+            strcpy(person.name,name);
+            strcpy(person.id, id);
+            person.age = age;
+            printf("posicao antes %ld\n", ftell(PeopleArch));
+            fwrite(&person, SIZE, 1, PeopleArch);
+            printf("posicao dps %ld\n", ftell(PeopleArch));
+            printf("size %d\n", SIZE);
+            regPeople++;
+            return;
+        }
+
+        // inserção ordenada
+        fclose(PeopleArch); 
+        PeopleArch = fopen("population.dat", "r+b");
+
+        char flag = 'C';
+        int auxPos;
+        fseek(PeopleArch, pos, 0);
+        int i = pos;
+        do {
+            fread(&person, SIZE, 1, PeopleArch);
+            auxPos = ftell(PeopleArch);
+            fread(&auxPerson, SIZE, 1, PeopleArch);
+            if (feof(PeopleArch)) {
+                printf("fim1\n"); //1 item deslocado
+                flag = 'B';
+            }
+            fseek(PeopleArch, auxPos, 0);
+            fwrite(&person, SIZE, 1, PeopleArch);
+            if (flag == 'B') {
+                printf("ALO\n");
+                break;
+            }
+            printf("ESCREVEU o 10\n");
+            auxPos = ftell(PeopleArch);
+            
+            fread(&person,SIZE,1,PeopleArch);
+            if (feof(PeopleArch)) {
+                printf("fim2\n"); 
+                flag = 'B';
+            }
+            fseek(PeopleArch, auxPos, 0);
+            fwrite (&auxPerson, SIZE, 1, PeopleArch);
+            if (feof(PeopleArch)) {
+                printf("fim3\n"); 
+                flag = 'B';
+            }
+            printf("SEGUNDO WRITE");
+            if (flag == 'B')
+                break;
+            
+        }            
+        while (flag != 'B');
+
+        
+        
+        fseek(PeopleArch, i, 0);
         strcpy(person.name,name);
         strcpy(person.id, id);
         person.age = age;
-        fseek(PeopleArch,0,2);
         fwrite(&person, SIZE, 1, PeopleArch);
         regPeople++;
+        fclose(PeopleArch);
+        PeopleArch = fopen("population.dat", "a+b");
+        return;
+               
+    }
 }
-    printf("Number of population: %ld\n", regPeople);
+
+
+void listAll() {
+    fseek(PeopleArch,0,0);
+
+    do {
+        fread(&person, SIZE, 1, PeopleArch);
+        if (feof(PeopleArch))
+            return;
+        printf("%s\n", person.name);
+        printf("%s\n", person.id);
+        printf("%d\n", person.age);
+        printf("-------------------\n\n");
+    }
+    while(!feof(PeopleArch));
 }
